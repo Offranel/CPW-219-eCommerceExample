e5201c5ea266da79637f9e92e34789e9dd367288
Offranel <francis.sengele@gmail.com>
2026-08-15 14:41:16 -0700

    Added Product class to eCommerce.Models with ProductId, Title, and Price properties. Applied [Key], [StringLength], and [Range] attributes for validation. Included XML documentation for class and properties.

---
+++ eCommerce/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Models;
+/// <summary>
+/// Represents an individual product for sale
+/// </summary>
+public class Product
+{
+    /// <summary>
+    /// The unique identifier for the product
+    /// </summary>
+    [Key]
+   
+    public int ProductId { get; set; }
+    /// <summary>
+    /// The user facing title of the product
+    /// </summary>
+
+    [StringLength(50, ErrorMessage = "Title must be at most 50 characters long.")]
+    public required string Title { get; set; }
+    /// <summary>
+    /// The current sales price of the product
+    /// </summary>
+
+    [Range(0, 10_000 )]
+    public double Price { get; set; }
+}
