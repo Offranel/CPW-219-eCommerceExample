f92a1a79e8777c1683cd42f94fb3abdf601c4d66
Offranel <francis.sengele@gmail.com>
2026-08-15 21:06:30 -0700

    Add ProductController and Create view for products
    Added ProductController with Index and Create actions for product listing and creation. Implemented Create.cshtml Razor view with form and validation for Title and Price. Set up for future database integration.

---
+++ eCommerce/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create ( Product p)
+
+        {
+            if (ModelState.IsValid)
+            {
+                // Add to database
+                // Redirect to Product list (Index action
+            }
+            return View(p);
+        }
+    }
+}
