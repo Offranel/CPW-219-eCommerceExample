fa36b4c96660075ed4209f7c16de64f5494aac06
Offranel <francis.sengele@gmail.com>
2026-08-16 20:24:25 -0700

    Enable DI for ProductDbContext; async Create action
    Added dependency injection for ProductDbContext in ProductController to support database access. Refactored the Create action to be asynchronous and implemented logic to add a Product and save changes asynchronously. Updated using directives to include eCommerce.Data.

--- eCommerce/Controllers/ProductController.cs
+++ eCommerce/Controllers/ProductController.cs
@@ -1,10 +1,17 @@
-﻿using eCommerce.Models;
+﻿using eCommerce.Data;
+using eCommerce.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerce.Controllers
 {
+    
     public class ProductController : Controller
     {
+        private readonly ProductDbContext _context;
+        public ProductController(ProductDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -16,15 +23,18 @@ namespace eCommerce.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create ( Product p)
+        public async Task<IActionResult> Create ( Product p)
 
         {
             if (ModelState.IsValid)
             {
-                // Add to database
-                // Redirect to Product list (Index action
+                
+                _context.Products.Add(p);// add the product to the context
+                await _context.SaveChangesAsync();// save changes to the database
+                
+                return RedirectToAction(nameof(Index));
             }
-            return View(p);
+            return View(p);// if model state is invalid, return the view with the product model to display validation errors
         }
     }
 }
