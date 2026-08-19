9b81c303a0688cd676c539f83da0590200dec833
Offranel <francis.sengele@gmail.com>
2026-08-18 18:57:45 -0700

    Add Product listing with async fetch and price support
    - Updated ProductController Index to fetch products asynchronously
    - Enhanced Product model with Price property and validation
    - Added navigation link to Product Listings in main layout
    - Created Index view to display products and add new ones

--- eCommerce/Controllers/ProductController.cs
+++ eCommerce/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using eCommerce.Data;
 using eCommerce.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerce.Controllers
 {
@@ -12,9 +13,10 @@ namespace eCommerce.Controllers
         {
             _context = context;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Product> allProducts = await _context.Products.ToListAsync();
+            return View(allProducts);
         }
         [HttpGet]
         public IActionResult Create()
