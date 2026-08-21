baf36a061ea2345629fdb1bfe399c83b30d69d8c
Offranel <francis.sengele@gmail.com>
2026-08-20 19:38:48 -0700

    Add RegistractionViewModel with validation annotations
    Added RegistractionViewModel class for member registration. Includes UserName, Email, Password, ConfirmPassword, and DateOfBirth properties with data annotations for input validation. Enforces validation rules using RegularExpression, StringLength, DataType, and Compare attributes.

--- eCommerce/Models/Member.cs
+++ eCommerce/Models/Member.cs
@@ -33,3 +33,39 @@ public class Member
     /// </summary>
     public DateOnly DateOfBirth { get; set; }
 }
+
+public class RegistractionViewModel
+{
+    /// <summary>
+    /// Public facing username for the member
+    /// Alphanumeric characteres only 
+    /// </summary>
+    [RegularExpression("^[a-zA-Z0-9]+$",
+        ErrorMessage = "Username must be alphanumeric only")]
+    [StringLength(25)]
+    public required string UserName { get; set; }
+    /// <summary>
+    /// Email for the member
+    /// </summary>
+    /// 
+    [DataType(DataType.EmailAddress)]
+    public required string Email { get; set; }
+    /// <summary>
+    /// The member password 
+    /// </summary>
+    /// 
+    [StringLength(50, MinimumLength = 6,
+        ErrorMessage = "Your password must be between 6 and 50 characteres")]
+    public required string Password { get; set; }
+
+    [Compare(nameof(Password))]
+    [DataType(DataType.Password)]
+    public required string ConfirmPassword {  get; set; }
+    /// <summary>
+    /// The date of birth
+    /// </summary>
+    [DataType(DataType.Date)]
+    public DateOnly DateOfBirth { get; set; }
+}
+
+
