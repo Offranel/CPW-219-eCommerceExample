7d850bd447701bd2242e502cba65736a7d41b43f
Offranel <francis.sengele@gmail.com>
2026-08-20 20:37:57 -0700

    Add member registration with DI and POST action
    Updated MemberController to use dependency injection for ProductDbContext. Added POST Register action for member registration with model validation, entity mapping, and database save. GET Register action retained for form display.

--- eCommerce/Controllers/MemberController.cs
+++ eCommerce/Controllers/MemberController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerce.Data;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
 
-namespace eCommerce.Controllers
+namespace eCommerce.Controllers;
+
+public class MemberController : Controller
 {
-    public class MemberController : Controller
+    private readonly ProductDbContext _context;
+
+    public MemberController(ProductDbContext context)
     {
-       public IActionResult Register()
-        {
-            return View();
+        _context = context;
+    }
+    public IActionResult Register()
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Register(RegistractionViewModel red)
+    {
+        if (ModelState.IsValid)
+        { 
+            //  Map View model
+            Member newmember = new()
+            {
+                UserName = red.UserName,
+                Email = red.Email,
+                Password = red.Password,
+                DateOfBirth = red.DateOfBirth,
+            };
+
+            _context.Members.Add(newmember);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
         }
+        return View(red);
     }
 }
