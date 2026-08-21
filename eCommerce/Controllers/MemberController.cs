b7cfb4e7ee5a95075593ae237c96d2f926bdff22
Offranel <francis.sengele@gmail.com>
2026-08-20 19:39:10 -0700

    Add MemberController with Register action
    Added MemberController to eCommerce.Controllers with a Register action method returning a view. Included Microsoft.AspNetCore.Mvc using directive.

---
+++ eCommerce/Controllers/MemberController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class MemberController : Controller
+    {
+       public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
