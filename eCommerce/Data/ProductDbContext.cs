9123fb24f61cd5f9c6e5cedf771ce3d6b732cf71
Offranel <francis.sengele@gmail.com>
2026-08-20 17:57:46 -0700

    Enforce unique constraints on Member fields
    Added unique indexes for UserName and Email in Member entity via OnModelCreating override. Introduced DbSet<Member> to ProductDbContext for entity tracking.

--- eCommerce/Data/ProductDbContext.cs
+++ eCommerce/Data/ProductDbContext.cs
@@ -8,6 +8,19 @@ public class ProductDbContext : DbContext
     public ProductDbContext(DbContextOptions options) : base(options)
     {
     }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Ensure the Member Username is unique
+        modelBuilder.Entity<Member>()
+            .HasIndex(m => m.UserName)
+            .IsUnique();
+        //Ensure the member Email is unique
+        modelBuilder.Entity<Member>()
+            .HasIndex(m => m.Email)
+            .IsUnique();
+    }
     // Entity to be tracked by DbContext
     public DbSet<Product> Products { get; set; }
+
+    public DbSet<Member> Members { get; set; }
 }
